8f6b650f3d910a84df24fc1870cc931d97889172
Janispe <46872148+Janispe@users.noreply.github.com>
2026-08-18 01:47:44 +0200

    fix: allow Thunderbird bridge requests

--- thunderbird_hausverwaltung/hooks.py
+++ thunderbird_hausverwaltung/hooks.py
@@ -15,6 +15,19 @@ doctype_js = {
 	"Mietvertrag": "public/js/mietvertrag.js",
 }
 
+before_request = [
+	"thunderbird_hausverwaltung.thunderbird_hausverwaltung.integrations.thunderbird_bridge.allow_extension_cors",
+]
+
+override_whitelisted_methods = {
+	"hausverwaltung.hausverwaltung.integrations.thunderbird_bridge.register_device": "thunderbird_hausverwaltung.thunderbird_hausverwaltung.integrations.thunderbird_bridge.register_device",
+	"hausverwaltung.hausverwaltung.integrations.thunderbird_bridge.list_devices": "thunderbird_hausverwaltung.thunderbird_hausverwaltung.integrations.thunderbird_bridge.list_devices",
+	"hausverwaltung.hausverwaltung.integrations.thunderbird_bridge.enqueue_search": "thunderbird_hausverwaltung.thunderbird_hausverwaltung.integrations.thunderbird_bridge.enqueue_search",
+	"hausverwaltung.hausverwaltung.integrations.thunderbird_bridge.enqueue_compose": "thunderbird_hausverwaltung.thunderbird_hausverwaltung.integrations.thunderbird_bridge.enqueue_compose",
+	"hausverwaltung.hausverwaltung.integrations.thunderbird_bridge.poll_command": "thunderbird_hausverwaltung.thunderbird_hausverwaltung.integrations.thunderbird_bridge.poll_command",
+	"hausverwaltung.hausverwaltung.integrations.thunderbird_bridge.acknowledge_command": "thunderbird_hausverwaltung.thunderbird_hausverwaltung.integrations.thunderbird_bridge.acknowledge_command",
+}
+
 scheduler_events = {
 	"hourly": [
 		"thunderbird_hausverwaltung.thunderbird_hausverwaltung.integrations.thunderbird_bridge.cleanup_commands",
